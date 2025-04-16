9ffdd267cb0364360219d3310b71ae1cff0c2f02
𝐒𝐀𝐑𝐄𝐆𝐀𝐌𝐀 𝐌𝐔𝐒𝐈𝐂 <bittulkr628@gmail.com>
2025-04-16 16:45:03 +0530

    saregamaMusic OP

--- SaregamaMusic/utils/thumbnails.py
+++ SaregamaMusic/utils/thumbnails.py
@@ -41,7 +41,7 @@ def truncate(text):
 
     text1 = text1.strip()
     text2 = text2.strip()     
-    return [text1, text2]
+    return [text1,text2]
 
 def random_color():
     return (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
@@ -100,24 +100,24 @@ def crop_center_circle(img, output_size, border, border_color, crop_scale=1.5):
     return result
 
 def draw_text_with_shadow(background, draw, position, text, font, fill, shadow_offset=(3, 3), shadow_blur=5):
-   
+    
     shadow = Image.new('RGBA', background.size, (0, 0, 0, 0))
     shadow_draw = ImageDraw.Draw(shadow)
-   
-  
+    
+    
     shadow_draw.text(position, text, font=font, fill="black")
-   
-  
+    
+    
     shadow = shadow.filter(ImageFilter.GaussianBlur(radius=shadow_blur))
-   
-  
+    
+    
     background.paste(shadow, shadow_offset, shadow)
     
-  
+    
     draw.text(position, text, font=font, fill=fill)
 
 
-
+    
 async def gen_thumb(videoid: str):
     try:
         if os.path.isfile(f"cache/{videoid}_v4.png"):
@@ -154,10 +154,10 @@ async def gen_thumb(videoid: str):
             else:
                 channel = "Unknown Channel"
 
-      
+        
         async with aiohttp.ClientSession() as session:
             async with session.get(thumbnail) as resp:
-             
+        
                 content = await resp.read()
                 if resp.status == 200:
                     content_type = resp.headers.get('Content-Type')
@@ -174,18 +174,18 @@ async def gen_thumb(videoid: str):
                     await f.write(await resp.read())
                     await f.close()
                     # os.system(f"file {filepath}")
-      
-      
+                    
+        
         image_path = f"cache/thumb{videoid}.png"
         youtube = Image.open(image_path)
         image1 = changeImageSize(1280, 720, youtube)
-       
+        
         image2 = image1.convert("RGBA")
         background = image2.filter(filter=ImageFilter.BoxBlur(20))
         enhancer = ImageEnhance.Brightness(background)
         background = enhancer.enhance(0.6)
 
-
+        
         start_gradient_color = random_color()
         end_gradient_color = random_color()
         gradient_image = generate_gradient(1280, 720, start_gradient_color, end_gradient_color)
@@ -208,7 +208,7 @@ async def gen_thumb(videoid: str):
         draw_text_with_shadow(background, draw, (text_x_position, 230), title1[1], title_font, (255, 255, 255))
         draw_text_with_shadow(background, draw, (text_x_position, 320), f"{channel}  |  {views[:23]}", arial, (255, 255, 255))
 
- 
+
         line_length = 580  
         line_color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
@@ -228,7 +228,7 @@ async def gen_thumb(videoid: str):
             circle_radius = 10 
             circle_position = (end_point_color[0], end_point_color[1])
             draw.ellipse([circle_position[0] - circle_radius, circle_position[1] - circle_radius,
-                          circle_position[0] + circle_radius, circle_position[1] + circle_radius], fill=line_color)
+                      circle_position[0] + circle_radius, circle_position[1] + circle_radius], fill=line_color)
     
         else:
             line_color = (255, 0, 0)
