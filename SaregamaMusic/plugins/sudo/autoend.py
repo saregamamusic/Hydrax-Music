9b8b3243dd0b41c8b451d88b8ab77d91f6f207e0
𝐒𝐀𝐑𝐄𝐆𝐀𝐌𝐀 𝐌𝐔𝐒𝐈𝐂 <bittulkr628@gmail.com>
2025-04-17 17:30:46 +0530

    Saregama Op

--- SaregamaMusic/plugins/sudo/autoend.py
+++ SaregamaMusic/plugins/sudo/autoend.py
@@ -13,12 +13,12 @@ async def auto_end_stream(_, message: Message):
     if len(message.command) != 2:
         return await message.reply_text(usage)
     state = message.text.split(None, 1)[1].strip().lower()
-    if state in ["enable", "on", "yes"]:
+    if state in ["enable","on","yes"]:
         await autoend_on()
         await message.reply_text(
             "» ᴀᴜᴛᴏ ᴇɴᴅ sᴛʀᴇᴀᴍ ᴇɴᴀʙʟᴇᴅ.\n\nᴀssɪsᴛᴀɴᴛ ᴡɪʟʟ ᴀᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟʏ ʟᴇᴀᴠᴇ ᴛʜᴇ ᴠɪᴅᴇᴏᴄʜᴀᴛ ᴀғᴛᴇʀ ғᴇᴡ ᴍɪɴs ᴡʜᴇɴ ɴᴏ ᴏɴᴇ ɪs ʟɪsᴛᴇɴɪɴɢ."
         )
-    elif state in ["disable", "off", "no"]:
+    elif state in ["disable","off","no"]:
         await autoend_off()
         await message.reply_text("» ᴀᴜᴛᴏ ᴇɴᴅ sᴛʀᴇᴀᴍ ᴅɪsᴀʙʟᴇᴅ.")
     else:
@@ -31,12 +31,12 @@ async def auto_leave_chat(_, message: Message):
     if len(message.command) != 2:
         return await message.reply_text(usage)
     state = message.text.split(None, 1)[1].strip().lower()
-    if state in ["enable", "on", "yes"]:
+    if state in ["enable","on","yes"]:
         await autoleave_on()
         await message.reply_text(
             "» ᴀᴜᴛᴏ leave chat ᴇɴᴀʙʟᴇᴅ.\n\nᴀssɪsᴛᴀɴᴛ ᴡɪʟʟ ᴀᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟʏ ʟᴇᴀᴠᴇ ᴛʜᴇ ᴠɪᴅᴇᴏᴄʜᴀᴛ ᴀғᴛᴇʀ ғᴇᴡ ᴍɪɴs ᴡʜᴇɴ ɴᴏ ᴏɴᴇ ɪs ʟɪsᴛᴇɴɪɴɢ."
         )
-    elif state in ["disable", "off", "no"]:
+    elif state in ["disable","off","no"]:
         await autoleave_off()
         await message.reply_text("» ᴀᴜᴛᴏ leave chat ᴅɪsᴀʙʟᴇᴅ.")
     else:
