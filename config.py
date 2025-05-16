647301fcef78649ac597a43efeeb1cbeeff44b27
𝐒𝐀𝐑𝐄𝐆𝐀𝐌𝐀 𝐌𝐔𝐒𝐈𝐂 <bittulkr628@gmail.com>
2025-05-16 19:38:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -43,8 +43,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", ""
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Hydrax_Music_Update")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/Hydrax_Music_Update")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "hhttps://t.me/natural1ura")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/itachixmusicupdatebot")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
